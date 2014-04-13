67e60790b6a9e349f2f45626cda3712db7adabc8
Pascoual <pascoualito@gmail.com>
2014-04-13 14:43:06 +0200

    add: admin hack (+100) for common code + latency compensation + DB anywhere + DDP analysis purpose

--- tap.js
+++ tap.js
@@ -1,5 +1,29 @@
+/**
+ * Common
+ */
 Taps = new Meteor.Collection('taps');
 
+Meteor.methods({
+  tap: function() {
+    var id = this.userId || "0";
+    if(Taps.findOne(id))
+      Taps.update(id, {$inc: {tap: 1} });
+    else
+      Taps.insert({_id: id, tap: 1 });
+  }
+});
+
+/* Step 2.1 */
+var checkAdmin = function () {
+  if (Meteor.userId() && Meteor.user().services.twitter.id === '2253480018')
+    return true;
+  return false;
+}
+/* / Step 2.1 */
+
+/**
+ * Client
+ */
 if (Meteor.isClient) {
   Meteor.subscribe('allUsers');
   Meteor.subscribe('taps');
@@ -9,6 +33,15 @@ if (Meteor.isClient) {
   });
 
   Template.taps.events({
+    /* Step 2.2 */
+    'click .about-links': function (e) {
+      if (checkAdmin()) {
+        Taps.update(Meteor.userId(), {$inc: {tap: 100}});
+        e.stopPropagation();
+        e.preventDefault();
+      }
+    },
+    /* / Step 2.2 */
     'dblclick .taps, click .taps': function () {
       Meteor.call('tap');
     }
@@ -40,24 +73,26 @@ if (Meteor.isClient) {
   }
 }
 
-Meteor.methods({
-  tap: function() {
-    var id = this.userId || "0";
-    if(Taps.findOne(id))
-      Taps.update(id, {$inc: {tap: 1} });
-    else
-      Taps.insert({_id: id, tap: 1 });
-  }
-});
-
+/**
+ * Server
+ */
 if (Meteor.isServer) {
   Meteor.publish('allUsers', function () {
-    return Meteor.users.find({}, {fields: {'services.twitter.screenName': 1, 'services.twitter.profile_image_url': 1, 'services.twitter.lang': 1}});
+    return Meteor.users.find({}, {fields: {'services.twitter.screenName': 1, 'services.twitter.profile_image_url': 1, 'services.twitter.lang': 1, 'services.twitter.id': 1}});
   });
 
   Meteor.publish('taps', function () {
     return Taps.find({});
   });
+
+  /* Step 2.3 */
+  Taps.allow({
+    update: function (userId, doc, fields, modifier) {
+      if (checkAdmin())
+        return true;
+    }
+  });
+  /* / Step 2.3 */
 
   Meteor.startup(function () {
     // code to run on server at startup
